b3fe42a46e27ac747b2938da3c5f49867a0f5db9
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-08 02:35:31 -0500

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
+import { Main, StickyFooter } from "../Theme";
+import Navigation from "./navigation";
 import "../scss/index.scss";
-// import Header from "./header";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -17,22 +18,12 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        <Navigation />
+        <Main>{children}</Main>
+        <StickyFooter>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+        </StickyFooter>
       </>
     )}
   />
