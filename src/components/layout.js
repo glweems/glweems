8a4c2456f44ec76787051e57cb00cd26b4dae19f
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-09 01:04:05 -0600

    Layout cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
-import { StickyLogo, Main, StickyFooter } from "../Theme";
+import styled from "styled-components";
+import { Theme, StickyFooter } from "../Theme";
+import { StickyLogo } from "../components/small-components";
 import Navigation from "./navigation";
 import "../scss/index.scss";
+
+const Main = styled.main`
+  padding-top: 2rem;
+  width: 100%;
+  overflow: hidden;
+  background: ${Theme.colors.bg};
+`;
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -20,7 +29,7 @@ const Layout = ({ children }) => (
       <>
         <Navigation />
         <Main>
-          <StickyLogo>glweems</StickyLogo>
+          <StickyLogo />
           {children}
         </Main>
         <StickyFooter>© {new Date().getFullYear()}, Built with</StickyFooter>
