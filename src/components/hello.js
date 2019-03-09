aa4b3d97d94e9b51556928c2fefcf35dd23e43aa
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-09 01:03:49 -0600

    Hello animated split

--- src/components/hello.js
+++ src/components/hello.js
@@ -4,20 +4,16 @@ import Theme from "../Theme";
 
 const Div = styled.div`
   width: 100%;
-  /* background: pink; */
+  margin-top: -2rem;
 `;
 
 const Split = styled.div`
-  // * Grid
   height: 80vh;
   display: flex;
-  .half {
-    /* height: 100%; */
-    width: 50%;
-  }
   .half:first-child {
     background: ${Theme.colors.red};
   }
+  transition: 1s all ease-in-out;
 `;
 
 const Msg = styled.h1`
@@ -26,24 +22,60 @@ const Msg = styled.h1`
   font-size: 10rem;
   font-style: italic;
   position: relative;
-  text-align: left;
+  text-align: center;
   top: 30%;
-  left: 5rem;
+  left: 50%;
 `;
 
 export default class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      grow: true,
+      scroll: window.scrollY,
+      width: 50
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll(event) {
+    this.setState({
+      grow: this.state.scroll > window.scrollY,
+      scroll: window.scrollY
+    });
+    if (!this.setState.scroll > window.screenY) {
+      this.setState({
+        width: this.state.width + window.scrollY / window.innerHeight,
+        scroll: window.scrollY
+      });
+    } else {
+      this.setState({
+        width: this.state.width - 1,
+        scroll: window.scrollY
+      });
+    }
+  }
+
   render() {
     return (
       <Div>
         <Split>
-          <div className="half">
+          <div className="half" style={{ width: `${this.state.width}%` }}>
             <Msg>
               He
               <br />
               llo
             </Msg>
           </div>
-          <div className="half" />
         </Split>
       </Div>
     );
