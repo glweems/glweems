96359d28f7fdd417f45861cf38003c73ba80bca8
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-12 06:06:38 -0400

    Config changes - changed eslint config to wesbos config. - package.json details added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,80 @@
+// import * as Sentry from '@sentry/browser';
+// import packageJson from './package.json';
+
+console.log('packageJson.version');
+// Sentry.init({});
+
+// const RELEASE = '0.1.0';
+
+// if (process.env.NODE_ENV === 'production') {
+//   Sentry.init({
+//     dsn: 'https://0c4da3a90ae54fb286dc83cd6daff483@sentry.io/1408965',
+//     release: RELEASE,
+//   });
+// }
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
-    title: `Designer / Developerr`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@glweems`
+    title: 'Designer / Developerr',
+    description:
+      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    author: '@glweems',
   },
   plugins: [
-    // in gatsby-config.js
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-root-import',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        '@': path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+      },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-source-graphql',
       options: {
-        name: `glweems-portfolio-site`,
-        short_name: `glweems`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
-  ]
+        typeName: 'GitHub',
+        fieldName: 'github',
+        url: 'https://api.github.com/graphql',
+        // HTTP headers
+        headers: {
+          Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+        },
+        // Additional options to pass to node-fetch
+        fetchOptions: {},
+      },
+    },
+    // ! SENTRY
+    {
+      resolve: 'gatsby-plugin-sentry',
+      options: {
+        dsn: 'https://0c4da3a90ae54fb286dc83cd6daff483@sentry.io/1408965',
+        environment: process.env.NODE_ENV,
+        enabled: (() =>
+          ['production', 'stage'].indexOf(process.env.NODE_ENV) !== -1)(),
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'glweems-portfolio-site',
+        short_name: 'glweems',
+        start_url: '/',
+        background_color: '#bada55',
+        theme_color: '#bada55',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+  ],
 };
