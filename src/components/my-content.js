1d828e3b8b9b615ceccadfa108f01bac8a18b8fe
Garrett Weems <gwgraphicdesign@gmail.com>
2019-04-08 13:41:17 -0400

    languages added to repos

--- src/components/my-content.js
+++ src/components/my-content.js
@@ -11,20 +11,21 @@ import {
   CardImg,
 } from 'src/styled/card'
 import Tags from '@/tags'
-import { Flex, IconLink } from 'styled/elements'
+import { Flex, Tag } from 'styled/elements'
 import Link from '@/link'
 
 export const MyTuts = ({ edges }) =>
   edges.map((edge, index) => (
-    <Card key={index} minwidth='245px'>
-      <Title>{edge.node.frontmatter.title}</Title>
-      <CardBody>
-        <CardImg img={edge.node.frontmatter.thumbnail} height='100px' />
-        <Flex hidden />
-        <Link to={edge.node.frontmatter.path}>Read More...</Link>
-      </CardBody>
-      <Tags {...edge.node.frontmatter} />
-    </Card>
+    <Link to={edge.node.frontmatter.path}>
+      <Card key={index} minwidth='245px'>
+        <Title>{edge.node.frontmatter.title}</Title>
+        <CardBody>
+          <CardImg img={edge.node.frontmatter.thumbnail} height='100px' />
+          <Flex hidden />
+        </CardBody>
+        <Tags {...edge.node.frontmatter} hashtag />
+      </Card>
+    </Link>
   ))
 
 MyTuts.propTypes = {
@@ -33,14 +34,15 @@ MyTuts.propTypes = {
 
 export const BehanceProjects = ({ edges }) =>
   edges.map((edge, i) => (
-    <Card key={i} minwidth='225px'>
-      <Title>{edge.node.name}</Title>
-      <CardBody>
-        <CardImg img={edge.node.covers.size_max_808} />
-        <Link to={`/designs/${edge.node.fields.slug}`}>See Project</Link>
-      </CardBody>
-      <Tags {...edge.node} />
-    </Card>
+    <Link to={`/designs/${edge.node.fields.slug}`}>
+      <Card key={i} minwidth='225px'>
+        <Title>{edge.node.name}</Title>
+        <CardBody>
+          <CardImg img={edge.node.covers.size_max_808} />
+        </CardBody>
+        <Tags {...edge.node} hashtag />
+      </Card>
+    </Link>
   ))
 
 BehanceProjects.propTypes = {
@@ -48,16 +50,24 @@ BehanceProjects.propTypes = {
 }
 export const PinnedRepos = ({ edges }) =>
   edges.map((edge, i) => (
-    <Card key={i} minwidth='225px'>
-      <Header>
-        <Title>{edge.node.name}</Title>
-        <IconLink href={edge.node.url}>
-          <FontAwesomeIcon icon={faGithub} />
-        </IconLink>
-      </Header>
-      <CardSubtitle>{edge.node.description}</CardSubtitle>
-      <Link href={edge.node.homepageUrl}>homepage</Link>
-    </Card>
+    <Link href={edge.node.homepageUrl}>
+      <Card key={i} minwidth='225px'>
+        <Header>
+          <Title>{edge.node.name}</Title>
+          <Link href={edge.node.url} target='_'>
+            <FontAwesomeIcon icon={faGithub} />
+          </Link>
+        </Header>
+        <CardSubtitle>{edge.node.description}</CardSubtitle>
+        <Flex scroll>
+          {edge.node.languages.edges.map((lang, i) => (
+            <Tag key={i} color={lang.node.color}>
+              {lang.node.name}
+            </Tag>
+          ))}
+        </Flex>
+      </Card>
+    </Link>
   ))
 
 PinnedRepos.propTypes = {
