86f81e5270128fc4a61ba4dd119995eaf1002ecc
Garrett Weems <gwgraphicdesign@gmail.com>
2019-04-08 13:40:42 -0400

    container added to blog-template.js

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -4,25 +4,11 @@ import { graphql } from 'gatsby'
 import Layout from '@/layout'
 import SEO from '@/seo'
 import 'prism-themes/themes/prism-atom-dark.css'
-// import 'prism-themes/themes/prism-base16-ateliersulphurpool.light.css'
-// import 'prism-themes/themes/prism-cb.css'
-// import 'prism-themes/themes/prism-darcula.css'
-// import 'prism-themes/themes/prism-duotone-dark.css'
-// import 'prism-themes/themes/prism-duotone-earth.css'
-// import 'prism-themes/themes/prism-duotone-forest.css'
-// import 'prism-themes/themes/prism-duotone-sea.css'
-// import 'prism-themes/themes/prism-duotone-space.css'
-// import 'prism-themes/themes/prism-ghcolors.css'
-// import 'prism-themes/themes/prism-hopscotch.css'
-// import 'prism-themes/themes/prism-pojoaque.css'
-// import 'prism-themes/themes/prism-xonokai.css'
+import { Container } from 'elements'
 import styled from 'styled-components'
 
 const BlogPost = styled.div`
   max-width: 100%;
-  * {
-    max-width: 100%;
-  }
 `
 
 const BlogTemplate = ({ data }) => {
@@ -33,9 +19,9 @@ const BlogTemplate = ({ data }) => {
     <Layout>
       <BlogPost>
         <SEO title={title} />
-        <div>
+        <Container>
           <article dangerouslySetInnerHTML={{ __html: html }} />
-        </div>
+        </Container>
       </BlogPost>
     </Layout>
   )
