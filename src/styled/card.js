6596487fd4965493e3b87722ceb88ae93ca67ac1
Garrett Weems <gwgraphicdesign@gmail.com>
2019-05-24 15:26:03 -0400

    card styling updates

--- src/styled/card.js
+++ src/styled/card.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components'
 import { Tag } from 'elements'
-import { lighten } from 'polished'
+import { lighten, darken } from 'polished'
 
 export const Card = styled.div`
   display: grid;
-  gap: 1rem;
+  gap: 0.25rem;
   grid-template-columns: 1fr;
-  grid-template-rows: 1fr auto 1.5em auto;
+  grid-template-rows: 1fr auto auto auto;
   min-width: ${props => (props.minwidth ? props.minwidth : null)};
   width: 100%;
-  padding: 0.5rem;
 
   *:not(${Tag}) {
     margin: 0;
@@ -18,12 +17,17 @@ export const Card = styled.div`
   .card-img {
     height: 125px;
     border-radius: 3px;
-    border: 2px solid ${props => props.theme.bg};
+    /* border: 2px solid ${props => props.theme.bg}; */
     :hover {
       border: 2px solid ${props => lighten(0.5, props.theme.text)};
     }
     transition: all 0.25s ease-in-out;
   }
+  background: ${props => darken(0.03, props.theme.bg)};
+  border: 2px solid ${props => darken(0.06, props.theme.bg)};
+  border-radius: 3px;
+  margin: 0.25rem 0.5rem;
+  padding: 0.25rem;
 `
 
 export const CardImg = styled.div`
