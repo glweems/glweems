6316545d1d72fd3399c2ecbfff37c039e4619ee7
Garrett Weems <gwgraphicdesign@gmail.com>
2019-08-16 18:05:11 -0500

    :bento: Updating logo / image assets

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,47 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes, faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
-import { Glweems } from './Icons';
+import { Logo } from './Icons';
 
-interface NavbarProps {
+interface Navbar {
   toggleMenu: React.Dispatch<React.SetStateAction<boolean>>;
   isMenu: boolean;
 }
 
-export const Header = styled.header`
+export const Header = styled.header<{ isMenu: boolean }>`
   z-index: 1000;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: ${props => props.theme.colors.bg};
-  svg {
-    fill: ${props => props.theme.colors.yellow};
-  }
+  opacity: 0.9;
+  padding: 0.25em;
+  background: ${props => props.theme.colors.dark};
   button {
-    color: ${props => props.theme.colors.muted};
+    color: ${props => props.theme.colors[!props.isMenu ? 'yellow' : 'red']};
   }
 `;
 
-const Navbar = ({ toggleMenu, isMenu }: NavbarProps) => (
-  <Header className="container-fluid">
-    <div>
-      <button type="button" onClick={() => toggleMenu(!isMenu)}>
-        <FontAwesomeIcon icon={isMenu ? faTimes : faBars} size="lg" />
-      </button>
-    </div>
-    <Link to="/">
-      <Glweems height="2em" />
-    </Link>
-    <div>
-      <button type="button">
-        <FontAwesomeIcon icon={faEnvelopeSquare} size="lg" />
-      </button>
-    </div>
-  </Header>
-);
+const Navbar = ({ toggleMenu, isMenu }: Navbar) => {
+  const toggle = () => {
+    toggleMenu(state => !state);
+  };
+
+  return (
+    <Header className="container-fluid" isMenu={isMenu}>
+      <div>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+      <div>
+        <button type="button" onClick={toggle}>
+          <FontAwesomeIcon icon={isMenu ? faTimes : faBars} size="2x" />
+        </button>
+      </div>
+    </Header>
+  );
+};
 
 export default Navbar;
