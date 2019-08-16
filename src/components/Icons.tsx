800992ac83ebc549bd702be406ab4c2982fd3f5b
Garrett Weems <gwgraphicdesign@gmail.com>
2019-08-16 18:07:58 -0500

    :bento: logo component created for nav

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { animated, useSpring } from 'react-spring';
+import Img from 'gatsby-image';
+import useGlweemsQuery from '../graphql/GlweemsQuery';
+
+export const Logo = () => (
+  <div style={{ padding: '.25em', marginBottom: '-.265em' }}>
+    <Img {...useGlweemsQuery().file.childImageSharp} />
+  </div>
+);
 
 const SVG = styled(animated.svg)`
   margin-top: 5px;
