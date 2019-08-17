73b804512bc7f1edb2279a0f901cb09b2901b919
Garrett Weems <gwgraphicdesign@gmail.com>
2019-08-17 04:50:44 -0500

    :sparkles: scroll position hooks

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,48 @@
+import { useRef, useLayoutEffect } from 'react';
+
 export const truncate = (input: string, target: number = 10) =>
   input.length > target - 3 ? `${input.substring(0, target - 3)}...` : input;
 
 export default { truncate };
+
+const isBrowser = typeof window !== `undefined`;
+
+function getScrollPosition({ element, useWindow }) {
+  if (!isBrowser) return { x: 0, y: 0 };
+
+  const target = element ? element.current : document.body;
+  const position = target.getBoundingClientRect();
+
+  return useWindow
+    ? { x: window.scrollX, y: window.scrollY }
+    : { x: position.left, y: position.top };
+}
+
+export function useScrollPosition(effect, deps, element, useWindow, wait) {
+  const position = useRef(getScrollPosition({ useWindow }));
+
+  let throttleTimeout = null;
+
+  const callBack = () => {
+    const currPos = getScrollPosition({ element, useWindow });
+    effect({ prevPos: position.current, currPos });
+    position.current = currPos;
+    throttleTimeout = null;
+  };
+
+  useLayoutEffect(() => {
+    const handleScroll = () => {
+      if (wait) {
+        if (throttleTimeout === null) {
+          throttleTimeout = setTimeout(callBack, wait);
+        }
+      } else {
+        callBack();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, deps);
+}
