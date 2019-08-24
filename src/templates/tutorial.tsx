c937c839720990694c6b1cde96be91813d11a794
Garrett Weems <gwgraphicdesign@gmail.com>
2019-08-24 16:32:45 -0500

    :lipstick: much better looking tutorial page

--- src/templates/tutorial.tsx
+++ src/templates/tutorial.tsx
@@ -1,40 +1,98 @@
 import * as React from 'react';
 import RehypeReact from 'rehype-react';
 import { graphql } from 'gatsby';
+import Image, { FluidObject } from 'gatsby-image';
+import styled from 'styled-components';
+import CodeSandbox from '../../../simple-codesandbox/dist/index';
 import SEO from '../components/SEO';
+
+type GitArray = [string, string, string, string?];
+
+interface Frontmatter {
+  date: string;
+  path: string;
+  title: string;
+  subtitle: string;
+  codesandbox: GitArray;
+  tags: string[];
+}
 
 interface BlogTemplateProps {
   data: {
     markdownRemark: {
       htmlAst: object;
-      frontmatter: {
-        title: string;
-        path: string;
-        date: string;
-        subtitle: string;
-        tags: string[];
+      frontmatter: Frontmatter;
+    };
+    file: {
+      childImageSharp: {
+        fluid: FluidObject;
       };
     };
   };
 }
 
+const Header = styled.header`
+  /* h1 {
+    color: ${props => props.theme.colors.dark};
+  } */
+`;
+
+const Article = styled.div`
+  display: grid;
+  grid-template-columns: auto 720px auto;
+  grid-template-rows: auto 1fr;
+  grid-template-areas: '. header .' '. article .';
+
+  ${Header} {
+    grid-area: header;
+  }
+
+  * {
+    grid-column: article;
+  }
+`;
+
 const BlogTemplate = ({
   data: {
     markdownRemark: {
       htmlAst,
-      frontmatter: { title, tags },
+      frontmatter: { date, path, title, subtitle, codesandbox, tags },
+    },
+    file: {
+      childImageSharp: { fluid: thumbnail },
     },
   },
-}: BlogTemplateProps) => {
-  const renderAst = new RehypeReact({
-    createElement: React.createElement,
-    components: {},
-  }).Compiler;
+}: BlogTemplateProps): JSX.Element => {
+  const PageElements = ({ elements }: { elements: unknown }) => {
+    const renderAst = new RehypeReact({
+      createElement: React.createElement,
+      components: {},
+    }).Compiler(elements);
+    return <>{renderAst.props.children}</>;
+  };
 
   return (
     <>
       <SEO title={title} keywords={tags} />
-      <article className="container">{renderAst(htmlAst)}</article>
+
+      <Article>
+        <Header>
+          <h1>{title}</h1>
+          <h2>{subtitle}</h2>
+          {codesandbox ? (
+            <CodeSandbox
+              git={codesandbox}
+              editorSize={40}
+              codeMirror
+              hideNavigation
+              view="preview"
+            />
+          ) : (
+            <Image fluid={thumbnail} />
+          )}
+        </Header>
+        <PageElements elements={htmlAst} />
+      </Article>
     </>
   );
 };
@@ -45,13 +103,22 @@ export const BlogPost = graphql`
   query MyQuery($slug: String!) {
     markdownRemark(frontmatter: { path: { eq: $slug } }) {
       frontmatter {
-        title
-        path
         date
+        path
+        title
         subtitle
+        codesandbox
+        thumbnail {
+          id
+        }
         tags
       }
       htmlAst
+    }
+    file(name: { in: ["tbn", $slug] }) {
+      childImageSharp {
+        ...FluidImage
+      }
     }
   }
 `;
