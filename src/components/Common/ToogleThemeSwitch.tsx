c6f25f97916214983bb56c103ea57db0ee8ef90e
Garrett Weems <gwgraphicdesign@gmail.com>
2019-09-15 23:54:58 -0500

    :bug: fix toggle button

--- src/components/Common/ToogleThemeSwitch.tsx
+++ src/components/Common/ToogleThemeSwitch.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-named-default */
 import React from 'react';
-import { ThemeContext } from 'styled-components';
+import { ThemeContext as StyledThemeContext } from 'styled-components';
 import { Button } from '.';
+import { ThemeContext } from '../Providers';
 
 export const ToogleThemeSwitch = () => {
-  const { theme, toggleTheme } = React.useContext(ThemeContext);
+  const theme = React.useContext(StyledThemeContext);
+  const { toggleTheme } = React.useContext(ThemeContext);
   return (
     <Button type="button" className="button" onClick={toggleTheme}>
       {theme.mode}
--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -3,3 +3,10 @@ import 'styled-components';
 
 export type GitHubCalendar = React.Component;
 declare module 'gatsby-image';
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    mode: 'dark' | 'light';
+  }
+}
